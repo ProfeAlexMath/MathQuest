c87a42b123d2b613d27835568f44f8f4ca8a3c84
alexme89 <54217200+alexme89@users.noreply.github.com>
2026-03-27 11:16:51 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'mathquest-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700;900&family=Exo+2:wght@300;400;600;700;800&family=Space+Mono:wght@400;700&display=swap'
+];
+
+// Instalación y almacenamiento en caché
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Estrategia: Carga desde caché y actualiza en segundo plano
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
